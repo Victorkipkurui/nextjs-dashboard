975096b385798f927023fe2013353b2fd034c22a
Victor Kipkurui <kipkurui.victor18@students.dkut.ac.ke>
2024-11-18 23:54:29 +0300

    midd

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
 export default function notFound () {
   return(
-    <h1>
+    <>
+    <h1 className="text-3xl font-bold items-center p-20">
        Page not found
     </h1>
+    <div className="items-center p-20">
+    <Link href='/'>
+      <Button className="bg-blue-600 text-white hover:bg-blue-900">Back to HomePage</Button>
+    </Link>
+    </div>
+    </>
+  
   )
 }
